92dffb7870fcd4ea5548c9552b0b5de207d70e2e
Nathan Manley <nathan.manley@gmail.com>
2019-09-12 13:24:58 -0600

    Update ubuntu1604.sh

--- script/install/ubuntu1604.sh
+++ script/install/ubuntu1604.sh
@@ -22,28 +22,10 @@ set -e
 apt update
 apt install -y software-properties-common
 apt-add-repository ppa:brightbox/ruby-ng -y
-apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xF1656F24C74CD1D8
-add-apt-repository 'deb [arch=amd64,i386,ppc64el] http://mirrors.coreix.net/mariadb/repo/10.1/ubuntu xenial main'
-curl -sL https://www.rabbitmq.com/rabbitmq-release-signing-key.asc | apt-key add -
-add-apt-repository 'deb http://www.rabbitmq.com/debian/ testing main'
 apt update
 export DEBIAN_FRONTEND=noninteractive
-apt install -y ruby2.3 ruby2.3-dev build-essential mariadb-server libmysqlclient-dev rabbitmq-server nodejs git nginx wget nano
+apt install -y ruby2.3 ruby2.3-dev build-essential libmysqlclient-dev nodejs git nginx wget nano
 gem install bundler procodile --no-rdoc --no-ri
-
-#
-# MySQL
-#
-echo 'CREATE DATABASE `postal` CHARSET utf8mb4 COLLATE utf8mb4_unicode_ci;' | mysql -u root
-echo 'GRANT ALL ON `postal`.* TO `postal`@`127.0.0.1` IDENTIFIED BY "p0stalpassw0rd";' | mysql -u root
-echo 'GRANT ALL PRIVILEGES ON `postal-%` . * to `postal`@`127.0.0.1`  IDENTIFIED BY "p0stalpassw0rd";' | mysql -u root
-
-#
-# RabbitMQ
-#
-rabbitmqctl add_vhost /postal
-rabbitmqctl add_user postal p0stalpassw0rd
-rabbitmqctl set_permissions -p /postal postal ".*" ".*" ".*"
 
 #
 # System prep
